d243c07b88962f92f95f9ed9bfbfe3df222bdeb3
Doubleiron <32973987+Doubleiron@users.noreply.github.com>
2018-01-21 21:29:22 +0800

    Add files via upload

---
+++ censor.py
@@ -0,0 +1,34 @@
+'''Write a function called censor that takes two strings, text and word, as input. It should return the text with the word you chose replaced with asterisks. For example:
+
+censor("this hack is wack hack", "hack")
+should return:
+
+"this **** is wack ****"
+Assume your input strings won't contain punctuation or upper case letters.
+The number of asterisks you put should correspond to the number of letters in the censored word.
+'''
+
+#censor1
+def censor(text,word):
+  text=text.split()
+  copy=[]
+  for item in text:
+    if item==word:
+      copy.append('*'*len(word))
+    else:
+      copy.append(item)
+  return " ".join(copy)
+
+#censor2
+def censor(text, word):
+    words = text.split()
+    result = ''
+    stars = '*' * len(word)
+    count = 0
+    for i in words:
+        if i == word:
+            words[count] = stars
+        count += 1
+    result =' '.join(words)
+
+    return result
